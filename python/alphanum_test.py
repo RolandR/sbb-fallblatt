b8b742e52d5f235aa20e8b5538bbfb90bc8aff87
Tobias Rueetschi <tr@brief.li>
2017-06-08 19:23:48 +0200

    flake8

--- python/alphanum_test.py
+++ python/alphanum_test.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import sbb_rs485
 import time
-from datetime import datetime
 
 DELAY_TIME = 4
 
@@ -15,6 +14,7 @@ POS_5 = 32
 LOG_OK = 1
 LOG_FAIL = 4
 
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -25,15 +25,24 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 def log(level, message):
-    if (level==1): pf="[{0}{1}{2}] ".format(bcolors.OKGREEN, "OK", bcolors.ENDC)
-    if (level==4): pf="[{0}{1}{2}]".format(bcolors.FAIL, "FAIL", bcolors.ENDC)
+    if (level == 1):
+        pf = "[{0}{1}{2}] ".format(bcolors.OKGREEN, "OK", bcolors.ENDC)
+    if (level == 4):
+        pf = "[{0}{1}{2}]".format(bcolors.FAIL, "FAIL", bcolors.ENDC)
     print("{0} {1}".format(pf, message))
 
 
 def fmt_ser(ser):
-    ss=str(hex(ser[0]))[2:].upper() + str(hex(ser[1]))[2:].upper() + str(hex(ser[2]))[2:].upper() + str(hex(ser[3]))[2:].upper()
+    ss = "{0}{1}{2}{3}".format(
+        str(hex(ser[0]))[2:].upper(),
+        str(hex(ser[1]))[2:].upper(),
+        str(hex(ser[2]))[2:].upper(),
+        str(hex(ser[3]))[2:].upper(),
+    )
     return ss
+
 
 def main():
     cc = sbb_rs485.PanelControl()
@@ -42,11 +51,11 @@ def main():
     exit = False
     while not exit:
         addr = input("Module address: {0}".format(bcolors.BOLD))
-        print(bcolors.ENDC, end="")
+        print(bcolors.ENDC)
         addr_int = int(addr)
 
         serial = cc.get_serial_number(addr_int)
-        if len(serial)==4:
+        if len(serial) == 4:
             log(LOG_OK, "reading serial ({0})".format(fmt_ser(serial)))
         else:
             log(LOG_FAIL, "reading serial")
@@ -85,11 +94,10 @@ def main():
 
         print("")
         inp = input("Test another module (Y/n): ")
-        if inp.lower()=="n":
+        if inp.lower() == "n":
             exit = True
         else:
             print("")
-
 
 
 if __name__ == '__main__':
