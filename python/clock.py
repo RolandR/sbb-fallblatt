02cc54a66e2d095f903ff0eb7b818aeef1459ebd
Tobias Rueetschi <tr@brief.li>
2017-07-12 21:18:34 +0200

    clock, add argparser

--- python/clock.py
+++ python/clock.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sbb_rs485
+import argparse
 import time
+import sbb_rs485
 from datetime import datetime
 
 SBB_MODULE_ADDR_HOUR = 55
@@ -10,11 +11,39 @@ SBB_MODULE_ADDR_MIN  = 56
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="Show a clock on SBB panels",
+    )
+    parser.add_argument(
+        '--port',
+        '-p',
+        help="Serial port",
+        type=str,
+        default='/dev/ttyUSB0',
+    )
+    parser.add_argument(
+        '--hour-address',
+        '-h',
+        help="Address of hour module",
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        '--minute-address',
+        '-m',
+        help="Address of minute module",
+        type=int,
+        default=1,
+    )
+    args = parser.parse_args()
+
     clock = sbb_rs485.PanelClockControl(
-        addr_hour=SBB_MODULE_ADDR_HOUR,
-        addr_min=SBB_MODULE_ADDR_MIN
+        port=args.port,
+        addr_hour=args.hour_address,
+        addr_min=args.minute_address,
     )
     clock.connect()
+
     while True:
         clock.set_time_now()
         ts = datetime.utcnow()
