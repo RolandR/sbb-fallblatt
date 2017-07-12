144f7a3d594e998aeb085a70c88d0c47cc542259
Tobias Rueetschi <tr@brief.li>
2017-07-12 21:00:00 +0200

    find_addr: use argparse to configure find addr run

--- python/find_addr.py
+++ python/find_addr.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
 import sbb_rs485
+
 
 POS_A = 0
 POS_E = 4
@@ -25,9 +27,17 @@ class bcolors:
 
 def log(level, message):
     if (level == 1):
-        pf = "[{0}{1}{2}] ".format(bcolors.OKGREEN, "OK", bcolors.ENDC)
+        pf = "[{0}{1}{2}] ".format(
+            bcolors.OKGREEN,
+            "OK",
+            bcolors.ENDC,
+        )
     if (level == 4):
-        pf = "[{0}{1}{2}]".format(bcolors.FAIL, "FAIL", bcolors.ENDC)
+        pf = "[{0}{1}{2}]".format(
+            bcolors.FAIL,
+            "FAIL",
+            bcolors.ENDC,
+        )
     print("{0} {1}".format(pf, message))
 
 
@@ -42,22 +52,52 @@ def fmt_ser(ser):
 
 
 def main():
-    cc = sbb_rs485.PanelClockControl()
-    cc.connect()
-    cc.serial.timeout = 2
+    parser = argparse.ArgumentParser(
+        description="Find SBB panel addresses",
+    )
+    parser.add_argument(
+        '--port',
+        '-p',
+        help="Serial port",
+        type=str,
+        default='/dev/ttyUSB0',
+    )
+    parser.add_argument(
+        '--start',
+        '-s',
+        help="Start address",
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        '--end',
+        '-e',
+        help="End address",
+        type=int,
+        default=255,
+    )
+    args = parser.parse_args()
 
-    input("Press any key")
-    for i in range(0, 256):
-        cc.set_addr_test(i)
-        print("Trying address {0}{1}{2}".format(bcolors.BOLD, i, bcolors.ENDC))
-        serial = cc.get_serial_number(i)
-        if len(serial) == 4:
-            print("Module address is: {0}{1}{2}".format(
+    cc = sbb_rs485.PanelControl(port=args.port)
+    cc.connect()
+    cc.serial.timeout = 0.05
+
+    try:
+        for i in range(args.start, args.end + 1):
+            print("Trying address {0}{1}{2}".format(
                 bcolors.BOLD,
                 i,
                 bcolors.ENDC,
             ))
-            break
+            serial = cc.get_serial_number(i)
+            if len(serial) == 4:
+                print("Module address is: {0}{1}{2}".format(
+                    bcolors.BOLD,
+                    i,
+                    bcolors.ENDC,
+                ))
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == '__main__':
