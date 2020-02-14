436dbd218cf945343b416d25b1b59ce99d832d45
Roland Rytz <roland.rytz@gmail.com>
2020-02-14 01:40:28 +0100

    make saysomething.sh use the fixed code

--- python/write.py
+++ python/write.py
@@ -24,7 +24,7 @@ def main():
     )
     args = parser.parse_args()
 
-    addrs = [96, 17, 7, 37, 38, 78, 35, 11, 14, 52, 36, 21, 10, 44, 5, 40]
+    addrs = [96, 17, 7, 37, 38, 78, 35, 11, 14, 52, 36, 21, 10, 44, 5, 40] #woo hardcoded
     cc = sbb_rs485.PanelAlphanumControl(addresses=addrs, port=args.port)
     cc.connect()
     cc.set_text(args.text)
