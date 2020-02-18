4defea674df7e0a4559ff59043547ae1b2459264
Roland Rytz <roland.rytz@gmail.com>
2020-02-19 00:52:03 +0100

    add script to rename our modules

---
+++ python/bort.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# used to rename our modules from addresses like 101, 102, 103, ... to 1, 2, 3, ...
+
+counter=1
+while [ $counter -le 30 ]
+do
+	echo "renaming $(($counter+100)) to $(($counter))"
+	
+	./change_addr.py -o $(($counter+100)) -n $(($counter))
+	((counter++))
+	
+	read -p "Press enter to continue"
+done
